3d3dac61e454eb8616805ad6eee4585b6e1556ca
saraswati Budha <sbudha@student.ysu.edu>
2023-08-11 22:11:33 +0000

    update

--- src/main/java/Application/DAO/FlightDAO.java
+++ src/main/java/Application/DAO/FlightDAO.java
@@ -33,7 +33,7 @@ public class FlightDAO {
         List<Flight> flights = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM FLIGHT";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
@@ -66,11 +66,13 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM FLIGHT WHERE flight_id=?;";
             
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setString and setInt methods here.
+            preparedStatement.setInt(1,id);     
+
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -108,12 +110,13 @@ public class FlightDAO {
         try {
             //Write SQL logic here. When inserting, you only need to define the departure_city and arrival_city
             //values (two columns total!)
-            String sql = "change me" ;
+            String sql = "Insert into flight (departure_city, arrival_city) values(?,?);" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             //write preparedStatement's setString and setInt methods here.
-
-
+            preparedStatement.setString(1,flight.departure_city);
+            preparedStatement.setString(2,flight.arrival_city);
+            
             preparedStatement.executeUpdate();
             ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
             if(pkeyResultSet.next()){
@@ -147,12 +150,14 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "UPDATE flight SET departure_city = ?, arrival_city = ? WHERE flight_id =?;";
+
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,flight.getDeparture_city());
+            preparedStatement.setString(2,flight.getArrival_city());
+            preparedStatement.setInt(3,id);
 
             //write PreparedStatement setString and setInt methods here.
-
-
             preparedStatement.executeUpdate();
         }catch(SQLException e){
             System.out.println(e.getMessage());
@@ -181,11 +186,13 @@ public class FlightDAO {
         List<Flight> flights = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM flight WHERE departure_city=? and arrival_city =?;";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write PreparedStatement setString and setInt methods here.
-
+            preparedStatement.setString(1,departure_city);
+            preparedStatement.setString(2, arrival_city);
+            
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,8 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        Flight f = flightDAO.insertFlight(flight);
+        return f;
     }
 
     /**
@@ -70,7 +71,14 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+        Flight f = new Flight();
+        if(flightDAO.getFlightById(flight_id)==null){
+            f = null;
+        }else{
+            flightDAO.updateFlight(flight_id, flight);
+            f = flightDAO.getFlightById(flight_id); 
+        }
+        return f;        
     }
 
     /**
@@ -80,7 +88,8 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        return flightDAO.getAllFlights();
+        //return f;
     }
 
     /**
@@ -92,6 +101,7 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+        return flightDAO.getAllFlights();
     }
+    
 }
